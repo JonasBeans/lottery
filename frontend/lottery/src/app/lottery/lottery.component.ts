e7ac143bf530ebfa3d825eb3615fdff75385e4a0
JonasBeans <boon.jonas.git@outlook.com>
2024-09-01 21:39:34 +0200

    Generate new components

---
+++ frontend/lottery/src/app/lottery/lottery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lottery',
+  standalone: true,
+  imports: [],
+  templateUrl: './lottery.component.html',
+  styleUrl: './lottery.component.css'
+})
+export class LotteryComponent {
+
+}
