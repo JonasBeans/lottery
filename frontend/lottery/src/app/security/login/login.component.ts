7c6bff1c755c540d856ee15ca4697a2e8d600668
JonasBeans <boon.jonas.git@outlook.com>
2024-09-08 10:25:22 +0200

    Feature: Introduce login components

---
+++ frontend/lottery/src/app/security/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatInputModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+login() {
+throw new Error('Method not implemented.');
+}
+
+}
